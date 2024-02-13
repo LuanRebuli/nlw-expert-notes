82e1738cecd0cf037b401f7f7ab45ca58a47b99a
Luan <yluanpvp@gmail.com>
2024-02-13 20:58:24 -0300

    resolve problem for upload react app

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,5 +1,4 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { transpose } from "date-fns";
 import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
